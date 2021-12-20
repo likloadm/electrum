6e77b32f2f72732ee3d9e66012203b52f48e5904
Василий <bobit300@gmail.com>
2021-12-20 14:27:58 +0300

    add tidecoin transactions

---
+++ contrib/android/p4a_recipes/tdc_yespower/__init__.py
@@ -0,0 +1,13 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+class BillBookLibRecipe(CompiledComponentsPythonRecipe):
+
+    site_packages_name = 'tdc_yespower'
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+
+
+    def should_build(self, arch):
+        return True
+
+recipe = BillBookLibRecipe()
