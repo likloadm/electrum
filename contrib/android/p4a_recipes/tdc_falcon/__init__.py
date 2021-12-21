87627d1771586d35ce544ccdd60513253fb6fe9c
Василий <bobit300@gmail.com>
2021-12-21 13:15:33 +0300

    add tidecoin transactions

--- contrib/android/p4a_recipes/tdc_falcon/__init__.py
+++ contrib/android/p4a_recipes/tdc_falcon/__init__.py
@@ -1,13 +1,8 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
-class BillBookLibRecipe(CompiledComponentsPythonRecipe):
+class TdcFalcon(CompiledComponentsPythonRecipe):
 
     site_packages_name = 'tdc_falcon'
     depends = ['setuptools']
-    call_hostpython_via_targetpython = False
 
-
-    def should_build(self, arch):
-        return True
-
-recipe = BillBookLibRecipe()
+recipe = TdcFalcon()
