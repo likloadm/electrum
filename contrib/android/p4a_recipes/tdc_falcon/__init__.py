076c4ac63d24b977227e4ab94e7940d9d220e30f
Василий <bobit300@gmail.com>
2021-12-21 23:28:17 +0300

    add tidecoin transactions

--- contrib/android/p4a_recipes/tdc_falcon/__init__.py
+++ contrib/android/p4a_recipes/tdc_falcon/__init__.py
@@ -4,5 +4,9 @@ class TdcFalcon(CompiledComponentsPythonRecipe):
 
     site_packages_name = 'tdc_falcon'
     depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+
+    def should_build(self, arch):
+        return True
 
 recipe = TdcFalcon()
--- contrib/android/p4a_recipes/tdc_yespower/__init__.py
+++ contrib/android/p4a_recipes/tdc_yespower/__init__.py
@@ -5,5 +5,10 @@ class TdcYespower(CompiledComponentsPythonRecipe):
     site_packages_name = 'tdc_yespower'
     depends = ['setuptools']
 
+    call_hostpython_via_targetpython = False
+
+    def should_build(self, arch):
+        return True
+
 
 recipe = TdcYespower()
