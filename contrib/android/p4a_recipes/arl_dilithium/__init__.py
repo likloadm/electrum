cabb68a377023298c663094226b113568210f91e
Vasilii <liklo.adm@gmail.com>
2022-05-21 23:27:54 +0300

    try fix arielcoin wallet

---
+++ contrib/android/p4a_recipes/arl_dilithium/__init__.py
@@ -0,0 +1,13 @@
+from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+
+class Kawpow(CompiledComponentsPythonRecipe):
+    version = 'master'
+    url = 'https://github.com/RavenCommunity/cpp-kawpow/archive/{version}.zip'
+    site_packages_name = 'kawpow'
+    depends = ['setuptools']
+    call_hostpython_via_targetpython = False
+
+    def should_build(self, arch):
+        return True
+
+recipe = Kawpow()
