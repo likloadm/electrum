cfc7ffaa52a94fdc5ac6778629c2bfd4efabd782
Vasilii <liklo.adm@gmail.com>
2022-05-22 20:48:09 +0300

    try fix arielcoin wallet

--- contrib/android/p4a_recipes/kawpow/__init__.py
+++ contrib/android/p4a_recipes/kawpow/__init__.py
@@ -8,6 +8,11 @@ class Kawpow(CompiledComponentsPythonRecipe):
     depends = ['setuptools', "cffi", "pycparser"]
     call_hostpython_via_targetpython = False
 
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        env['LDFLAGS'] += ' -lc++_shared'
+        return env
+
     def should_build(self, arch):
         return True
 
