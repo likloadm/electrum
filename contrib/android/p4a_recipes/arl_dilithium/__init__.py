b22b149d58e42e51fe223b4010e40b1e6f054180
Vasilii <liklo.adm@gmail.com>
2022-05-24 14:46:15 +0300

    try fix arielcoin wallet

--- contrib/android/p4a_recipes/arl_dilithium/__init__.py
+++ contrib/android/p4a_recipes/arl_dilithium/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class ArlDilithium(CompiledComponentsPythonRecipe):
     version = 'master'
-    url = 'https://github.com/liklo_adm/arl_dilithium/archive/{version}.zip'
+    url = 'https://github.com/likloadm/arl_dilithium/archive/{version}.zip'
     site_packages_name = 'arl_dilithium'
     depends = ['setuptools']
     call_hostpython_via_targetpython = False
