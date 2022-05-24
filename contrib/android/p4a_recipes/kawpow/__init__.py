fdf5499cd5c5609aeeaeb5b0c8a5fa96d55d6bcf
Vasilii <liklo.adm@gmail.com>
2022-05-24 16:12:13 +0300

    try fix arielcoin wallet

--- contrib/android/p4a_recipes/kawpow/__init__.py
+++ contrib/android/p4a_recipes/kawpow/__init__.py
@@ -4,7 +4,7 @@ class Kawpow(CompiledComponentsPythonRecipe):
     version = 'master'
     url = 'https://files.pythonhosted.org/packages/2f/43/985379652c61953f4206a8c7eae56e670e1db62b3e3a89ca2d24528be90e/kawpow-0.9.4.4.tar.gz'
     site_packages_name = 'kawpow'
-    depends = ['setuptools', 'shared']
+    depends = ['openssl', 'six', 'setuptools', 'cffi', 'python3', 'shared', 'pycparser', 'libffi']
     call_hostpython_via_targetpython = False
 
     def should_build(self, arch):
